6061cc1d4451bc6623b41442682740823a8568da
hamid reza <126154017+hamidreza1983@users.noreply.github.com>
2025-12-19 00:11:28 +0100

    edit message

--- core/messages.py
+++ core/messages.py
@@ -7,22 +7,34 @@ from PyQt5.QtWidgets import QMessageBox
 class MessageBox:
 
     @staticmethod
-    def success(parent, text, title="success full"):
-        QMessageBox.information(parent, title, text)
+    def success(parent, text, title="Success", on_ok=None):
+        msg = QMessageBox(parent)
+        msg.setIcon(QMessageBox.Information)
+        msg.setWindowTitle(title)
+        msg.setText(text)
+        if msg.exec_() == QMessageBox.Ok:
+            if on_ok:
+                on_ok()
 
     @staticmethod
-    def warning(parent, text, title="warning"):
-        QMessageBox.warning(parent, title, text)
-        
-    @staticmethod
-    def error(parent, text, title="error"):
-        QMessageBox.critical(parent, title, text)
+    def error(parent, text, title="error", on_ok=None):
+        msg = QMessageBox(parent)
+        msg.setIcon(QMessageBox.Critical)
+        msg.setWindowTitle(title)
+        msg.setText(text)
+        if msg.exec_() == QMessageBox.Ok:
+            if on_ok:
+                on_ok()
 
     @staticmethod
-    def info(parent, text, title="info"):
-        QMessageBox.information(parent, title, text)
-
-        MessageBox.success()
+    def warning(parent, text, title="warning", on_ok=None):
+        msg = QMessageBox(parent)
+        msg.setIcon(QMessageBox.Warning)
+        msg.setWindowTitle(title)
+        msg.setText(text)
+        if msg.exec_() == QMessageBox.Ok:
+            if on_ok:
+                on_ok()
         
         
         
