e1dbc06842a31c63340e0a9da28bb9321f8f8cda
mehrnooshgholami66 <mehrnoosh.gholami.66@gmail.com>
2025-12-21 00:51:55 +0330

    Update professor.py

--- ui_form/professor.py
+++ ui_form/professor.py
@@ -80,11 +80,11 @@ class ProfessorForm(object):
 
     # ----------------- متد انتخاب فایل -----------------
     def select_file(self):
-       file_path,_= QFileDialog.getOpenFileName(self.form,"select file","","All Files(*.*)")
-if not file_path:
-    return
-self.selected_file_path=file_path
-self.browselabel_uplod.setText(str(file_path))
+       file_path, _ = QFileDialog.getOpenFileName(self.form, "select file", "", "All Files(*.*)")
+       if not file_path:
+           return
+       self.selected_file_path = file_path
+       self.browselabel_uplod.setText(str(file_path))
 
 
     # ----------------- متد آپلود فایل -----------------
